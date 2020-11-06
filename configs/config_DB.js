1f0a4aca1c500f3e08bf660329d110842f800312
nghiatvh <nghia17042001@gmail.com>
2020-11-06 23:46:59 +0700

    #

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,6 +1,7 @@
 module.exports = {
     localdb: {
-        urldb : 'mongodb+srv://nghiatvh:nghia123456xf@cluster0.uinee.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv:nghiatvh:nghia123456@cluster0.c0xty.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        
         dbname : "atn-shop",
         dbusername : "nghiatvh",
         dbpassword : "nghia123456",
