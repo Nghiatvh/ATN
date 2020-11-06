4a1086be4149840d84480637a191e3697e195570
nghiatvh <nghia17042001@gmail.com>
2020-11-06 23:11:34 +0700

    update

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://nghiatvh:nghia123456xf@cluster0.uinee.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "nghiatvh",
+        dbpassword : "nghia123456",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://nghiatvh:nghia123456xf@cluster0.uinee.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "nghiatvh",
+        dbpassword : "nghia123456",
+    }
+
+};
